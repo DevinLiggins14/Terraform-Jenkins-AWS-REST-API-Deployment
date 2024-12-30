4b5bda90fb9bb9d0b707db5469345b284e899368
Devin Liggins <dlliggins14@gmail.com>
2024-12-30 18:09:13 -0500

    Update backend_s3.tf

--- worker_scripts/terraform_jenkins/backend_s3.tf
+++ worker_scripts/terraform_jenkins/backend_s3.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "dev-proj-1-jenkins-remote-state-bucket-123456"
-    key            = "terraform/state"
-    region         = "ap-south-1" # Update this to match the bucket's actual region
-    encrypt        = true
+    bucket = "terraform-backend6"
+    key    = "devops-project-1/terraform.tfstate"
+    region = "us-east-1"
   }
 }
