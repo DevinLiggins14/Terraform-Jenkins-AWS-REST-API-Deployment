bb5bc06ff9103528211ec7a0dd3c09f53dbc3ad3
Devin Liggins <dlliggins14@gmail.com>
2025-01-05 14:41:58 -0500

    Update main.tf

--- worker_scripts/app_infrastructure/networking/main.tf
+++ worker_scripts/app_infrastructure/networking/main.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "dev_proj_1_public_subnets" {
   count             = length(var.cidr_public_subnet)
   vpc_id            = aws_vpc.dev_proj_1_vpc_us_east_2.id
   cidr_block        = element(var.cidr_public_subnet, count.index)
-  availability_zone = element(var.us_east_2_availability_zone, count.index)
+  availability_zone = element(var.us_availability_zone, count.index)
 
   tags = {
     Name = "dev-proj-public-subnet-${count.index + 1}"
@@ -45,7 +45,7 @@ resource "aws_subnet" "dev_proj_1_private_subnets" {
   count             = length(var.cidr_private_subnet)
   vpc_id            = aws_vpc.dev_proj_1_vpc_us_east_2.id
   cidr_block        = element(var.cidr_private_subnet, count.index)
-  availability_zone = element(var.us_east_2_availability_zone, count.index)
+  availability_zone = element(var.us_availability_zone, count.index)
 
   tags = {
     Name = "dev-proj-private-subnet-${count.index + 1}"
