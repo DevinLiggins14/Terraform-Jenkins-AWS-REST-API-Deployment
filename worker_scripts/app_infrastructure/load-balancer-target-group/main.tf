03b65e3cdf7ee18a12d50cd05b6550a367d92355
Devin Liggins <dlliggins14@gmail.com>
2025-01-05 15:28:54 -0500

    Update main.tf

--- worker_scripts/app_infrastructure/load-balancer-target-group/main.tf
+++ worker_scripts/app_infrastructure/load-balancer-target-group/main.tf
@@ -1,15 +1,30 @@
-module "load-balancer" {
-  source                    = "../load-balancer"  # Corrected to relative path
-  lb_name                   = "dev-proj-1-alb"
-  is_external               = false
-  lb_type                   = "application"
-  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
-  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
-  tag_name                  = "dev-proj-1-alb"
-  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
-  ec2_instance_id           = module.ec2.dev_proj_1_ec2_instance_id
-  lb_listner_port           = 5000
-  lb_listner_protocol       = "HTTP"
-  lb_listner_default_action = "forward"
-  lb_target_group_attachment_port = 5000
+variable "lb_target_group_arn" {}
+variable "ec2_instance_id" {}
+variable "lb_target_group_port" {}
+
+resource "aws_lb_target_group" "dev_proj_1_lb_target_group" {
+  name     = "dev-proj-1-lb-target-group"
+  port     = var.lb_target_group_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+  }
+
+  tags = {
+    Name = "dev-proj-1-lb-target-group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
+  target_group_arn = var.lb_target_group_arn
+  target_id        = var.ec2_instance_id
+  port             = var.lb_target_group_port
 }
